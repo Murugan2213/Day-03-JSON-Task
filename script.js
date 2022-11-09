4e949e13f17248c89fc9b0d3f150e483be360f62
unknown <murugan2213@gmail.com>
2022-11-09 10:32:12 +0530

    JSON Task

---
+++ script.js
@@ -0,0 +1,33 @@
+let resume = {
+    "Personal Details":
+    {
+        "Name": "Murugan.R",
+        "e-mail": "murugan2213@gamil.com",
+        "Mobile": 9042948284,
+        "Degree": "B.Sc Computer Science",
+        "Location": {
+            "Address": "B-309, Casagrand Woodside, Manapakkam",
+            "Pincode": 600125,
+            "City": "Chennai",
+            "State": "Tamilnadu",
+            "Country": "India"
+        },
+    },
+
+    "Experience":
+    {
+        "Company": "Straive",
+        "Position": "Senior Executive - Graphics",
+        "StartDate": "19-11-2014",
+        "EndDate": "Till Working",
+    },
+
+    "Education":
+    {
+        "Collage": "Muthurangam Govt.Arts College",
+        "Department": "B.Sc Compueter Science",
+        "Year": "2002-2005",
+        "Percentage": "75%"
+    },
+}
+console.log(resume);
